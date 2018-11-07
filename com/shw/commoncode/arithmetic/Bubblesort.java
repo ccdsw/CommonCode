8c60071ca50fca2bca0a4dfe5e3142df0d39fc7d
shiwei <196774737@qq.com>
2018-11-07 14:48:21 +0800

    arithmetic

---
+++ com/shw/commoncode/arithmetic/Bubblesort.java
@@ -0,0 +1,19 @@
+package com.shw.commoncode.arithmetic;
+
+/**
+ * Created by shiwei on 18-11-7.
+ */
+
+public class Bubblesort {
+    public void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
